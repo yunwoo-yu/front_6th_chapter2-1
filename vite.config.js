2650772da1cd03e76fb30549ffeed5a73ce5343b
Ycow <skypnal12@gmail.com>
2025-08-01 03:10:08 +0900

    build: deploy gh-pages 추가

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,33 @@
 import { fileURLToPath, URL } from 'node:url';
 
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import path from 'path';
 import { defineConfig } from 'vite';
 
+const base = process.env.NODE_ENV === 'production' ? '/front_6th_chapter2-1/' : '';
+
+const entryFileName = 'index.advanced.html';
+
 export default defineConfig({
-  plugins: [react()],
+  base,
+  //2.
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, entryFileName),
+    },
+  },
+  plugins: [
+    react(),
+    {
+      name: 'rename-html-output',
+      closeBundle() {
+        const from = path.resolve(__dirname, `dist/${entryFileName}`);
+        const to = path.resolve(__dirname, 'dist/index.html');
+        if (fs.existsSync(from)) fs.renameSync(from, to);
+      },
+    },
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src/advanced', import.meta.url)),
